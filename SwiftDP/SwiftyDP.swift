7d8434c7c5212a788e5747777597eadf1d1a87de
ehowlsla <jungler@cvdv.me>
2016-04-27 16:19:33 +0900

    1080 * 1920 dp(ldpi) to IPhone PPI

---
+++ SwiftDP/SwiftyDP.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftyDP.swift
+//  SwiftDP
+//
+//  Created by YoonTaesup on 2016. 4. 27..
+//  Copyright © 2016년 YoonTaesup. All rights reserved.
+//
+
+import UIKit
+
+postfix operator ^^ {}
+
+public postfix func ^^(dp: CGFloat) -> CGFloat {
+    return dp * CGFloat(0.7710843373494)
+}
+
--- SwiftDP/ViewController.swift
+++ SwiftDP/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var y = CGFloat(50)
+        for i in 5..<10 {
+            let btn = UIButton(type: .Custom)
+            btn.frame = CGRectMake(20^^, y + 20^^, self.view.frame.width - 40^^, CGFloat(i*10)^^)
+            btn.layer.borderColor = UIColor.darkTextColor().CGColor
+            btn.layer.borderWidth = 1
+            btn.setTitleColor(UIColor.darkTextColor(), forState: .Normal)
+            btn.setTitle("Height \(i*10)dp", forState: .Normal)
+            self.view.addSubview(btn)
+            
+            y = btn.frame.maxY
+        }
     }
 
     override func didReceiveMemoryWarning() {
